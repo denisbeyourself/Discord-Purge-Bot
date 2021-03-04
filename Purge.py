2f622c070f0e2b85c70af254950ee44575b47b26
denisser121 <61875961+denisbeyourself@users.noreply.github.com>
2021-03-04 21:33:47 +0200

    Add files via upload

---
+++ Purge.py
@@ -0,0 +1,30 @@
+prefix = "!"
+token =  "Your Token Here"
+
+import asyncio
+import discord
+from discord.ext import commands
+from discord.ext.commands import bot
+import pyfiglet
+from pyfiglet import Figlet
+
+client = commands.Bot(command_prefix=prefix, self_bot=True)
+
+custom_fig = Figlet(font='graffiti')
+print(custom_fig.renderText('purge bot'))
+
+@client.event
+async def on_ready():
+    print ("Username: {}#{}".format(client.user.name, client.user.discriminator))
+    print ("How to use: !purge [amount]")
+
+@client.command()
+async def purge(ctx, amount: int):
+    await ctx.message.delete()
+    async for message in ctx.message.channel.history(limit=amount).filter(lambda m: m.author == client.user).map(lambda m: m):
+        try:
+           await message.delete()
+        except:
+            pass
+
+client.run(token, bot=False)
